9a113bc46480c7f0f7bf93e9f7e525460a523a65
pol <polfalguera@DESKTOP-4PLUJVT.localdomain>
2023-02-23 20:34:46 +0100

    P77907

--- Haskell/Sessio1/P77907-Functions_with_numbers.hs
+++ Haskell/Sessio1/P77907-Functions_with_numbers.hs
@@ -8,9 +8,10 @@ power x p = x * power x (p-1)
 isPrime :: Int -> Bool
 isPrime n
     | n <= 1 = False
-    | otherwise = res
+    | otherwise = (length divisors) == 0 -- otherwise = res
     where
-        res = null [x | x <- [2..(floor (sqrt (fromIntegral n)))],mod n x == 0]
+        divisors = [x | x <- [2..(floor (sqrt (fromIntegral n)))],mod n x == 0]
+        -- res = null [x | x <- [2..(floor (sqrt (fromIntegral n)))],mod n x == 0]
 
 slowFib :: Int -> Int
 slowFib 0 = 0
@@ -25,3 +26,5 @@ quickFib n = auxFib 0 1 n
 auxFib :: Int -> Int -> Int -> Int
 auxFib n1 n2 0 = n1
 auxFib n1 n2 iteration = auxFib n2 (n1 + n2) (iteration - 1) 
+
+
